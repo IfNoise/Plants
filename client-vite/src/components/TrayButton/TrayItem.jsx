8da88320938bad1a5d1721096a4af4b398facb27
IfNoise <black_russan@mail.ru>
2024-01-13 16:53:15 +0700

    изменено:      client-vite/src/components/TrayButton/TrayItem.jsx

--- client-vite/src/components/TrayButton/TrayItem.jsx
+++ client-vite/src/components/TrayButton/TrayItem.jsx
@@ -1,17 +1,47 @@
 import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
-import { Button, Card, Typography } from '@mui/material';
+import { Button, Card, Popover, Typography } from '@mui/material';
+import { useState } from 'react';
 
 
 
 export const TrayItem=({plant})=>{
-
+  const [openPopup,setOpenPopup]=useState(false)
+ const onMouseEnterHandler=()=>{
+  setOpenPopup(true)
+ }
+ const onMouseLeaveHandler=()=>{
+  setOpenPopup(false)
+ }
 
   return(
 
     <>
-    <Card  display="block" sx={{m:"2px",p:'1px',maxWidth:"60px",backgroundColor:'lightseagreen'}} >
+    <Card  display="block" 
+      sx={{
+        m:"2px",
+        p:'1px',
+        maxWidth:"60px"
+        ,backgroundColor:'lightseagreen'
+      }}
+      onMouseEnter={onMouseEnterHandler}
+      onMouseLeave={onMouseLeaveHandler}
+     >
       <Typography display="block" sx={{mb:0,p:0,fontSize:9}} variant='overline'>{plant?.pheno}</Typography>
     </Card>
+    <Popover open={openPopup}>
+      <Typography variant='h6'>
+        {plant?.strain}
+      </Typography>
+      <Typography variant='h4'>
+        {plant?.pheno}
+      </Typography>
+      <Typography variant='caption'>
+        {plant?.type}
+      </Typography>
+      <Typography variant='h5'>
+        Start:{plant?.start}
+      </Typography>
+    </Popover>
     
     </>
 
