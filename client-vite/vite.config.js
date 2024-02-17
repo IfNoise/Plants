5ca49a3c6761c957ad3da544a4c16f35af478184
IfNoise <black_russan@mail.ru>
2024-02-17 19:01:33 +0700

    изменено:      client-vite/index.html 	изменено:      client-vite/package-lock.json 	изменено:      client-vite/package.json 	новый файл:    client-vite/pwa-assets.config.js 	изменено:      client-vite/vite.config.js 	изменено:      package-lock.json 	изменено:      package.json

--- client-vite/vite.config.js
+++ client-vite/vite.config.js
@@ -1,11 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate',
+    injectRegister: 'auto',
+    manifest: {
+      "name": "Plants Database",
+      "short_name": "Plants Database",
+      "start_url": "/",
+      "display": "standalone",
+      "theme_color": "#000000",
+      "background_color": "#ffffff",
+      "icons": [
+        {
+          "src": "/icons/android-chrome-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "/icons/android-chrome-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ]
+    },
+  
+  })
+  ],
   server: {
     hmr: {
         host: "localhost"
