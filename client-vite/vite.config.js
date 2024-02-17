0656d07e709c126dc493d6d746bed110fba55ef9
IfNoise <black_russan@mail.ru>
2024-02-17 20:38:38 +0700

    изменено:      client-vite/vite.config.js

--- client-vite/vite.config.js
+++ client-vite/vite.config.js
@@ -22,15 +22,21 @@ export default defineConfig({
       "background_color": "#ffffff",
       "icons": [
         {
-          "src": "/icons/android-chrome-192x192.png",
+          "src": "pwa-64x64.png",
+          "sizes": "64x64",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-192x192.png",
           "sizes": "192x192",
           "type": "image/png"
         },
         {
-          "src": "/icons/android-chrome-512x512.png",
+          "src": "pwa-512x512.png",
           "sizes": "512x512",
           "type": "image/png"
-        }
+        },
+        
       ]
     },
   
