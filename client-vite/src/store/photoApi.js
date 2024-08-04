77b7e39feed02bd2bd3798c66764dc597625b44c
IfNoise <black_russan@mail.ru>
2024-08-04 23:46:49 +0700

    изменено:      client-vite/src/store/photoApi.js

--- client-vite/src/store/photoApi.js
+++ client-vite/src/store/photoApi.js
@@ -1,18 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { baseUrl } from "../config/config";
 
-const photoApi = createApi({
+export const photoApi = createApi({
   reducerPath: "photo/api",
   baseQuery: fetchBaseQuery({
     baseUrl: baseUrl + "/photos",
     refetchOnFocus: true,
-    prepareHeaders: (headers, { getState }) => {
-      const token = getState().auth.token;
-      if (token) {
-        headers.set("authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
+    // prepareHeaders: (headers, { getState }) => {
+    //   const token = getState().auth.token;
+    //   if (token) {
+    //     headers.set("authorization", `Bearer ${token}`);
+    //   }
+    //   return headers;
+    // },
   }),
   endpoints: (build) => ({
     uploadPhotos: build.mutation({
