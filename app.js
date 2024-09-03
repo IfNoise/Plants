b8cf04af5098c448729e68a03a68d2d1bd103a03
IfNoise <black_russan@mail.ru>
2024-09-03 17:45:46 +0700

    изменено:      app.js

--- app.js
+++ app.js
@@ -60,7 +60,6 @@ app.get('/api/photos/make_thumbnails', async(req, res) => {
   photos.forEach(async (photo) => {
     const filename= photo.src?.includes("gallery/")?photo.src.split("/")[1]:photo.src
     console.log("Filename",filename)
-    if(fs.existsSync(`uploads/thumbnails/${filename}`))return
     const path="uploads/"+filename
     console.log("Path:",path)
     const thumb=`uploads/thumbnails/${filename}`
